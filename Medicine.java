068e3848e2f5b502b0aa664da49c65757fe14f87
YUSU189501 <149136627+YUSU189501@users.noreply.github.com>
2024-03-27 10:14:00 +0900

    セルフメディケーション税制計算スクリプト
    セルフメディケーション税制計算スクリプト

--- Medicine.java
+++ Medicine.java
@@ -1,45 +1,98 @@
 import java.io.*;
 
-class MedIcine {
-	public static void main(String[] args) {
-		String Filename = "/home/suzuki/DATA/Medicine1";
-		try {
-			FileWriter out = new FileWriter(Filename);
-			String lf = System.getProperty("line.separator");
-			out.write("20230102,◆パブロンゴールド,1280" + lf);
-			out.write("20230102,*キリン生茶,433" + lf);
-			out.write("20230107,*キリン生茶,433" + lf);
-			out.write("20230107,*シマダヤ流水麺,218" + lf);
-			out.write("20230107,キレイキレイ,498" + lf);
-			out.write("20230107,キュキュット詰め替え用,297" + lf);
-			out.write("20230207,◆パブロンゴールド,1280" + lf);
-			out.write("20230207,キレイキレイ,498" + lf);
-			out.write("20230207,*サントリクラフトボス,218" + lf);
-			out.write("20230207,キュキュット詰め替え用,297" + lf);
-			out.write("20230211,*アサヒ三ツ矢サイダー,137" + lf);
-			out.write("20230227,キュキュット詰め替え用,297" + lf);
-			out.write("20230227,キレイキレイ,498" + lf);
-			out.write("20230307,◆パブロン鼻炎カプセル,1280" + lf);
-			out.write("20230307,*サントリクラフトボス,218" + lf);
-			out.write("20230312,◆パブロンゴールド,1280" + lf);
-			out.write("20230319,◆パブロン鼻炎カプセル,1280" + lf);
-			out.write("20230319,キュキュット詰め替え用,297" + lf);
-			out.write("20230319,キレイキレイ,498" + lf);
-			out.write("20230325,◆パブロン鼻炎カプセル,1280" + lf);
-			out.write("20230331,◆パブロン鼻炎カプセル,1280" + lf);
-			out.write("20230403,キュキュット詰め替え用,297" + lf);
-			out.write("20230403,キレイキレイ,498" + lf);
-			out.write("20230411,◆パブロン鼻炎カプセル,1280" + lf);
-			out.write("20230411,*サントリクラフトボス,218" + lf);
-			out.write("20230415,◆パブロンゴールド,1280" + lf);
-			out.write("20230419,◆パブロン鼻炎カプセル,1280" + lf);
-			out.write("20230419,キュキュット詰め替え用,297" + lf);
-			out.write("20230419,キレイキレイ,498" + lf);
-			out.write("20230423,◆パブロン鼻炎カプセル,1280" + lf);
-			out.write("20230429,◆パブロン鼻炎カプセル,1280" + lf);
-			out.close();
-		} catch (IOException e) {
-			System.out.println(e);
-		}
-	}
+class Ryosyusyo {
+    //privateフィールド
+	private int date;
+    private String merchandise;
+    private int price;
+    
+	//コンストラクタ
+    public Ryosyusyo(int d, String m, int p) {
+        date = d;
+        merchandise = m;
+        price = p;
+    }
+    
+	//日付を取得
+    public int getDate() {
+        return date;
+    }
+    
+	//商品名を取得
+    public String getMerchandise() {
+        return merchandise;
+    }
+    
+	//価格を取得
+    public int getPrice() {
+        return price;
+    }
+    
+	//ファイル書き込み用
+    public String CSV() {
+        return date + "," + merchandise + "," + price;
+    }
+}
+
+class Medicine {
+    public static void main(String[] args) {
+        //オブジェクト生成
+    	Ryosyusyo[] ryosyusyo = {
+            new Ryosyusyo(20230102, "◆パブロンゴールド", 1280),
+            new Ryosyusyo(20230102, "*キリン生茶", 433),
+            new Ryosyusyo(20230107, "*キリン生茶", 433),
+            new Ryosyusyo(20230107, "*シマダヤ流水麺", 218),
+            new Ryosyusyo(20230107, "キレイキレイ", 498),
+            new Ryosyusyo(20230107, "キュキュット詰め替え用", 297),
+            new Ryosyusyo(20230207, "◆パブロンゴールド", 1280),
+            new Ryosyusyo(20230207, "キレイキレイ", 498),
+            new Ryosyusyo(20230207, "*サントリクラフトボス", 218),
+            new Ryosyusyo(20230207, "キュキュット詰め替え用", 297),
+            new Ryosyusyo(20230211, "*アサヒ三ツ矢サイダー", 137),
+            new Ryosyusyo(20230227, "キュキュット詰め替え用", 297),
+            new Ryosyusyo(20230227, "キレイキレイ", 498),
+            new Ryosyusyo(20230307, "◆パブロン鼻炎カプセル", 1280),
+            new Ryosyusyo(20230307, "*サントリクラフトボス", 218),
+            new Ryosyusyo(20230312, "◆パブロンゴールド", 1280),
+            new Ryosyusyo(20230319, "◆パブロン鼻炎カプセル", 1280),
+            new Ryosyusyo(20230319, "キュキュット詰め替え用", 297),
+            new Ryosyusyo(20230319, "キレイキレイ", 498),
+            new Ryosyusyo(20230325, "◆パブロン鼻炎カプセル", 1280),
+            new Ryosyusyo(20230331, "◆パブロン鼻炎カプセル", 1280),
+            new Ryosyusyo(20230403, "キュキュット詰め替え用", 297),
+            new Ryosyusyo(20230403, "キレイキレイ", 498),
+            new Ryosyusyo(20230411, "◆パブロン鼻炎カプセル", 1280),
+            new Ryosyusyo(20230411, "*サントリクラフトボス", 218),
+            new Ryosyusyo(20230415, "◆パブロンゴールド", 1280),
+            new Ryosyusyo(20230419, "◆パブロン鼻炎カプセル", 1280),
+            new Ryosyusyo(20230419, "キュキュット詰め替え用", 297),
+            new Ryosyusyo(20230419, "キレイキレイ", 498),
+            new Ryosyusyo(20230423, "◆パブロン鼻炎カプセル", 1280),
+            new Ryosyusyo(20230429, "◆パブロン鼻炎カプセル", 1280)
+        };
+        
+    	// ファイル指定
+        String Filename = "/mnt/dev8/FILE/Medicine1";
+        try {
+            // ファイルへの書き込み
+            BufferedWriter out = new BufferedWriter(new FileWriter(Filename));
+            String lf = System.getProperty("line.separator");
+            for(Ryosyusyo ryo : ryosyusyo) {
+                out.write(ryo.CSV() + lf);
+            }
+            out.close();
+            
+            // ファイルからの読み取り
+            FileReader in = new FileReader(Filename);
+            BufferedReader br = new BufferedReader(in);
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+            br.close();
+            
+        } catch (IOException e) {
+            System.out.println("ファイルが書き込み中にエラーが発生しました。");
+        }
+    }
 }
--- Medicine.sh
+++ Medicine.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #カレントディレクトリを設定する。
-cd ~ && pwd
+cd /mnt/dev8 && pwd
 
 # Medを実行。
-java MedIcine
+java Medicine
 
 # Javaファイルを指定する。
-INFILE="/home/suzuki/DATA/Medicine1"
+INFILE="/mnt/dev8/FILE/Medicine1"
 
 # 税率と税込額を算出する。
-OUTFILE1="/home/suzuki/DATA/Medicine2"
+OUTFILE1="/mnt/dev8/FILE/Medicine2"
 cat "${INFILE}" | awk -v  OFS=, -F, '{tax = ($1<"20191001"||$2~"^*") ? 1.08 : 1.1; print $0,tax ;}' |
  awk -v OFS=, -F, '{print $0, int($3*$4)}{a+=int($3*$4)}END{print "合計額",a}' > "${OUTFILE1}"
 
@@ -18,7 +18,7 @@ echo "税率・税込額ファイル"
 cat "${OUTFILE1}"
 
 # セルフメディケーション税制額を算出する。
-OUTFILE2="/home/suzuki/DATA/SELF-MED-1"
+OUTFILE2="/mnt/dev8/FILE/SELF-MED-1"
 cat "${OUTFILE1}" | awk -v  OFS=, -F, '{tax = ($2~"^*") ? 1.08 : 1.1; print $0,tax}' | awk -v OFS=, -F, '$2 ~ /^◆/' |
  awk -v OFS=, -F, '{print int($3*$4)}' | numsum > "${OUTFILE2}"
 
